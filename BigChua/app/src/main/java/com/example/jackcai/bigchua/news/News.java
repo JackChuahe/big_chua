92f9301720c89a294080ab9a5fe56d06580eef59
jackchua <jackcaihe@outlook.com>
2016-04-26 16:49:00 +0800

    sliderbar完成,但是无法设置点击事件

--- BigChua/app/src/main/java/com/example/jackcai/bigchua/news/News.java
+++ BigChua/app/src/main/java/com/example/jackcai/bigchua/news/News.java
@@ -18,9 +18,15 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
 import com.example.jackcai.bigchua.R;
 import com.example.jackcai.bigchua.UserVerify.LoginActivity;
 import com.example.jackcai.bigchua.utils.DownLoadImage;
@@ -44,7 +50,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class News extends Fragment implements AdapterView.OnItemClickListener {
+public class News extends Fragment implements AdapterView.OnItemClickListener , BaseSliderView.OnSliderClickListener{
 
     private ListView listView;
     private List<NewsModel> newsModelList;
@@ -53,6 +59,8 @@ public class News extends Fragment implements AdapterView.OnItemClickListener {
     private ProgressBar loadPb;
     private static String URL_HEAD = "http://c.m.163.com/nc/article/headline/T1348647909107/0-20.html?from=toutiao&size=20&passport=&devId=tJ9WJmNCbemaYAkn9hmvNg%3D%3D&lat=30T6CPAny3Qjfrm2aZ0Iyg%3D%3D&lon=TdJ6mqRwrgAvXDYm6qorxw%3D%3D&version=5.4.3&net=wifi&ts=1450750816&sign=JYtJRfoZ%2FZUob1Ul08iHiZVsutvrccBfEGkTdNMwqyl48ErR02zJ6%2FKXOnxX046I&encryption=1&canal=baidu_news";
     private static String URL_NEWS_LIST = "http://r.inews.qq.com/getQQNewsUnreadList?uid=9bbb976dec28f6ae&omgbizid=bb4c10d2d92de041fc6a894637deac8ac5700050210b16&Cookie=%20lskey%3D%3B%20luin%3D%3B%20skey%3D%3B%20uin%3D%3B%20logintype%3D0%20&qn-rid=391487196&store=5&hw=TiantianVM_TianTian&devid=950790062947205&qn-sig=46f4c335f6311193104bf7d6218161b4&user_chlid=news_news_finance%2Cnews_news_ent%2Cnews_news_sports%2Cnews_news_tech%2Cnews_news_ssh%2Cnews_news_mil%2Cnews_news_lad&screen_width=600&mac=67%253Afd%253A0e%253A46%253A86%253A40&last_time=1450752215&chlid=news_news_top&appver=18_android_4.8.7&qqnetwork=wifi&forward=0&page=0&last_id=NEW2015122201243000&mid=fb7dd60a37d004a54e059fd9613257b8f2457515&imsi=310260835531535&omgid=13b925aa2f9a5446a1fabb239e0df59cb60a0010210b16&apptype=android&screen_height=1024";
+    private SliderLayout sliderLayout;
+    private RelativeLayout relativeLayout;
 
     @Nullable
     @Override
@@ -61,6 +69,15 @@ public class News extends Fragment implements AdapterView.OnItemClickListener {
         listView = (ListView)view.findViewById(R.id.news_list_view);
         loadPb = (ProgressBar)view.findViewById(R.id.news_list_pb);
         loadPb.setVisibility(View.VISIBLE);
+
+
+        LayoutInflater headerInflater= LayoutInflater.from(getContext());
+        View headerView = headerInflater.inflate(R.layout.news_img_slider,null);
+        relativeLayout = (RelativeLayout) headerView.findViewById(R.id.header_frame);
+        sliderLayout = (SliderLayout) headerView.findViewById(R.id.news_imgs_slider);
+
+
+        listView.addHeaderView(relativeLayout);
         loadData();
         return view;
     }
@@ -104,9 +121,22 @@ public class News extends Fragment implements AdapterView.OnItemClickListener {
             listView.setAdapter(adapter);
             listView.setOnItemClickListener(this);
         }else {//头部加载完成
+            loadHeader();
         }
     }
 
+
+    public void loadHeader(){
+        sliderLayout.setCustomAnimation(new DescriptionAnimation());
+        sliderLayout.setPresetTransformer(SliderLayout.Transformer.Default);
+        sliderLayout.setPresetIndicator(SliderLayout.PresetIndicators.Right_Bottom);
+        for (AdsModel model : adsModelList){
+            TextSliderView textSliderView = new TextSliderView(this.getActivity());
+            textSliderView.description(model.getTitle());
+            textSliderView.image(model.getImgSrc());
+            sliderLayout.addSlider(textSliderView);
+        }
+    }
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         if (position == newsModelList.size()){
@@ -116,6 +146,15 @@ public class News extends Fragment implements AdapterView.OnItemClickListener {
         intent.putExtra("title",newsModelList.get(position).getTitle());
         intent.putExtra("url",newsModelList.get(position).getContentUrl());
         startActivity(intent);
+    }
+
+    /**
+     * 滑动项被点击
+     * @param slider
+     */
+    @Override
+    public void onSliderClick(BaseSliderView slider) {
+        Toast.makeText(getContext(),"dsaad",Toast.LENGTH_SHORT).show();
     }
 }
 
@@ -271,7 +310,7 @@ class AsynGetNewsData extends AsyncTask<String,Void,String>{
             if (type == 0){
                 news.setModelList(modelList,null,type);
             }else{
-                news.setModelList(null,adsModelList,0);
+                news.setModelList(null,adsModelList,1);
             }
             news.loadDataFinished(type);// 加载数据完成后 回调数据
         }else{
